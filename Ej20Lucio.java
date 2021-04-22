52edd3931045d04cae1cff9cad71992e0e80eb63
LucioQuirelli <81364234+LucioQuirelli@users.noreply.github.com>
2021-04-22 10:06:05 -0300

    Add files via upload

---
+++ Ej20Lucio.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1lucio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc08
+ */
+public class Ej20Lucio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+      System.out.println("Ingrese un numero y le mostraré los múltiplos de 6 que hay hasta llegar al mismo.");
+      int num = teclado.nextInt();
+      int i = 1;
+       while (i <= num){
+           if (i % 6 == 0){
+               System.out.println(i); 
+           }
+               i++;
+       }
+    }
+    
+}
---
+++ Ej21Lucio.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1lucio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc08
+ */
+public class Ej21Lucio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+      System.out.println("Ingrese un numero y le mostraré los múltiplos de 6 que hay hasta llegar al mismo.");
+      int num = teclado.nextInt();
+      int i = 1;
+      for (i= 0; i<= num; i++){
+           if (i % 6 == 0){
+               System.out.println(i); 
+           }
+       }
+    }
+    
+}
