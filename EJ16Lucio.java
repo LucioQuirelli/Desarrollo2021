4395574083d41e369a03ba2bcadaf5efb0e9a47a
LucioQuirelli <81364234+LucioQuirelli@users.noreply.github.com>
2021-04-22 09:30:01 -0300

    Add files via upload

---
+++ EJ16Lucio.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1lucio;
+
+/**
+ *
+ * @author pc08
+ */
+public class EJ16Lucio {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+      
+       for (int num=0; num<=100; num++){
+           if (num % 2 == 0 && num % 3 == 0 ){
+               System.out.println(num); 
+           }
+              
+       }
+              
+    }
+    
+}
---
+++ Ej18Lucio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1lucio;
+
+/**
+ *
+ * @author pc08
+ */
+public class Ej18Lucio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         for (int num=0; num<=2000; num++){
+           if (num % 10 == 0){
+               System.out.println(num); 
+           }
+              
+       }
+    }
+    
+}
